3cc93eb824e5991f0999d22984f5c38b23355867
huangwj <huangwjwork@gmail.com>
2018-04-17 23:09:51 +0800

    123

--- dingding_webhook/alert_for_ELK/README.md
+++ dingding_webhook/alert_for_ELK/README.md
@@ -1,9 +1,63 @@
-监测ELK中JAVA日志等级并告警
-[toc]
+# 监测ELK中JAVA日志等级并告警
+实现监控ELK中JAVA日志并推送所有非INFO信息，推送通过钉钉实现
+## 模块
 分两个模块：
 * 查询告警
 * 恢复模块
-## 查询
-1. 查询非INFO的日志  hostname，tag，local_time
-1. lostname_tag 作为唯一名 local_time 和 loglevel作为属性
-1. 告警策略，收到ERROR且数据库中不存在该ERROR或时间
+
+## 查询模块
+* 查询非INFO的日志，对比数据库中该项目的trigger状态
+* 若为trigger则刷新trigger时间
+* 若为resolved则发出告警并记录告警的相关参数
+* 告警消息通过钉钉机器人推送
+
+## 恢复模块
+* 遍历数据库中trigger状态
+* 若距离上一次trigger刷新时间大于一定时间间隔，则发出恢复信息
+
+## 相关设计
+### 告警数据
+告警数据以json格式存储在elasticsearch中，`_index=wyny_elk`,`_type=alert`,`_id={hostname}_{tags}`,详细格式如下：
+```json
+PUT /wyny_elk/alert/hadoop1_namenode
+{
+  "log_time":"2018-04-15 03:32:42 123",
+  "log_level":"INFO",
+  "trigger_status":"alert"
+}
+```
+查看elasticsearch告警信息:
+```json
+GET /wyny_elk/alert/hadoop1_namenode
+
+{
+  "_index": "wyny_elk",
+  "_type": "alert",
+  "_id": "hadoop1_namenode",
+  "_version": 1,
+  "found": true,
+  "_source": {
+    "log_time": "2018-04-15 03:32:42 123",
+    "log_level": "INFO",
+    "trigger_status": "alert"
+  }
+}
+```
+### 告警消息推送
+推送至钉钉自定机器人，格式为markdown
+```json
+webhook_message = {
+"msgtype": "markdown",
+"markdown": {
+    "title":"markdown测试",
+    "text":{
+# ELK日志告警
+hostname: **{hostname}**
+log_level: **{log_level}**
+log_time: **{log_time}**
+log_messgae: **log_message**
+}
+}
+}
+```
+
--- dingding_webhook/alert_for_ELK/alert_for_ELK.py
+++ dingding_webhook/alert_for_ELK/alert_for_ELK.py
@@ -7,9 +7,12 @@
 # time: 2018/4/9 21:39
 
 from elasticsearch import Elasticsearch
+import time
+import datetime
 import json
 import requests
 # dingding
+time1 = time.time()
 webhook_url = "https://oapi.dingtalk.com/robot/send?access_token=45f4b695b1dbe35998626e9083ca8b212d475943259273a699127f1cc76edadb"
 
 webhook_header = {
@@ -73,6 +76,21 @@ query_json_test = {
 # query_string = "nginx.access.remote_ip:172.16.8.241 AND nginx.access.remote_ip:172.16.8.241"
 search_number = elastic_client.count(body=query_json_test)
 print(search_number['count'])
+elasticsearch_scroll_info = elastic_client.search(scroll='1m',body=query_json_test)
+scroll_id = elasticsearch_scroll_info['_scroll_id']
+print(scroll_id)
+scroll_size = elasticsearch_scroll_info['hits']['total']
+print(scroll_size)
+while scroll_size > 0:
+    print("scrolling...")
+    elasticsearch_scroll = elastic_client.scroll(scroll_id=scroll_id,scroll='1m')
+    scroll_id = elasticsearch_scroll['_scroll_id']
+    print(scroll_id)
+    scroll_size = len(elasticsearch_scroll['hits']['hits'])
+    print("scroll_size:"+str(scroll_size))
+    for  hit in elasticsearch_scroll['hits']['hits']:
+        print(hit['_id'])
+
 
 # quert_string = 'nginx.access.method:GET AND nginx.access.remote_ip:172.16.8.241'
 
@@ -80,6 +98,8 @@ print(search_number['count'])
 #
 # with open('search.json',mode='w') as aaa:
 #     aaa.write(json.dumps(elastic_response,indent=1))
+time2=time.time()
+print(time2-time1)
 
 
 
